bc42836ae53c7159f5843dcce3b558ce14f65dd0
МакСаша <mack.pavlencko2016@gmail.com>
2026-03-17 22:58:48 +0200

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // ДОДАЙ ЦЕЙ РЯДОК (це і є "домен", про який я казав):
+  site: 'https://example.com', 
+  
+  integrations: [icon()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
